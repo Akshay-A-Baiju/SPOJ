43c23839e5d9e573adac90545ebebe34e987a1e0
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-08-12 08:38:21 +0530

    Create Divisor Summation.cpp
    #number theory

---
+++ Number Theory/Divisor Summation.cpp
@@ -0,0 +1,65 @@
+// Author : Akshay A Baiju
+// Problem link : https://www.spoj.com/problems/DIVSUM/
+// Problem tags : number theory
+// TC : O(t*sqrt(n))
+// SC : O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll                  long long
+#define ull                 unsigned long long
+#define vi                  vector <int>
+#define vll                 vector <ll>
+#define vvll                vector <vll>
+#define pii                 pair <int,int>
+#define pll                 pair <ll,ll>
+#define ms(a,x)             memset (a,x,sizeof(a))
+#define rep(i,a,n)          for (int i=a;i<n;i++)
+#define rrep(i,n,a)         for (int i=n;i>=a;i--)
+#define pb                  push_back
+#define pf                  push_front
+#define ppb                 pop_back
+#define umap                unordered_map
+#define uset                unordered_set
+#define all(v)              v.begin(), v.end()
+#define desc()              greater <ll>()
+#define endl                "\n"   //not to be used in interactive problems
+#define scan_vector(v,n)    for (auto &it: v) { cin>>it; }
+#define print_vector(v)     for (auto &it: v) { cout<<it<<" "; } cout<<endl;
+#define print_map(mp)       for (auto &pr: mp) { cout<<pr.first<<" "<<pr.second<<endl; }
+#define sync                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const int M = 1e9+7;
+
+void solve()
+{
+    ll n,i,sum=1;
+    cin>>n;
+    if (n==1)
+    {
+        cout<<0;
+        return;
+    }
+    for (i=2;i*i<n;i++)
+    {
+        if (n%i==0)
+            sum+=i+(n/i);
+    }
+    if (i*i==n)
+        sum+=i;
+    cout<<sum;
+}
+
+int main()
+{
+    clock_t clk = clock();
+    sync;
+    int t=1;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+        cout<<endl;
+    }
+    cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    return 0;
+}
